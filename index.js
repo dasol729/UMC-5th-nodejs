5ea5789a528ae7bd2088cb9c9a9d51b60e4890ff
dasol729 <97594634+dasol729@users.noreply.github.com>
2023-11-09 13:04:08 +0900

    8주차 실습

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 // npm run start
-const express = require('express')   // common JS
-// import express from 'express';         // ES6
+//const express = require('express');   // common JS
+import express from 'express';         // ES6
+import { tempRouter } from './src/routes/temp.route.js';
+import { BaseError } from "./config/error.js";
+import { status } from './config/response.status.js';
+import { response } from './config/response.js';
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
+// router setting
+app.use('/temp', tempRouter);
+
+// error handling
+app.use((req, res, next) => {
+    const err = new BaseError(status.NOT_FOUND);
+    next(err);
+});
+app.use((err, req, res, next) => {
+    // 템플릿 엔진 변수 설정
+    res.locals.message = err.message;   
+    // 개발환경이면 에러를 출력하고 아니면 출력하지 않기
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {}; 
+    res.status(err.data.status).send(response(err.data));
+});
+
+app.listen(port, () => {
+		console.log(`Example app listening on port ${port}`);
+});
+
+
+/*
 const mysql = require("mysql2");
 
 const db = require('./models');
@@ -42,4 +68,6 @@ app.get('/delete', (req, res) => {
     user.destroy({where: {id: 18}});
     res.send('delete');
 });
+*/
+
 
